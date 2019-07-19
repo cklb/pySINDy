9a2c8e97e57b159b0d5abb690b577e9a44efcce6
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2019-07-19 11:19:20 +0200

    Added first draft for control input support prediction
    The prediction is not yet optimized since an evaluation mode for the
    model is missing in which only the elements in the theta matrix are
    computed that correspond to nonzero weights.

--- pySINDy/sindy.py
+++ pySINDy/sindy.py
@@ -1,9 +1,15 @@
 """
 Derived module from sindybase.py for classical SINDy
 """
+import logging
 import numpy as np
+from scipy.interpolate import interp1d
+from scipy.integrate import solve_ivp
 from findiff import FinDiff
 from .sindybase import SINDyBase
+
+
+logger = logging.getLogger(__name__)
 
 
 class SINDy(SINDyBase):
@@ -11,7 +17,8 @@ class SINDy(SINDyBase):
     Sparse Identification of Nonlinear Dynamics:
     reference: http://www.pnas.org/content/pnas/113/15/3932.full.pdf
     """
-    def fit(self, data, _dt, poly_degree=2, cut_off=1e-3, deriv_acc=2):
+    def fit(self, data, _dt, poly_degree=2, cut_off=1e-3, deriv_acc=2,
+            input_dim=0):
         """
         :param data: dynamics data to be processed
         :param _dt: float, represents grid spacing
@@ -20,6 +27,9 @@ class SINDy(SINDyBase):
         :param deriv_acc: (positive) integer, derivative accuracy
         :return: a SINDy model
         """
+        self._deg = poly_degree
+        self._inp_dim = input_dim
+
         if len(data.shape) == 1:
             data = data[np.newaxis, ]
 
@@ -33,7 +43,7 @@ class SINDy(SINDyBase):
 
         # compute time derivative
         d_dt = FinDiff(data.ndim-1, _dt, 1, acc=deriv_acc)
-        x_dot = d_dt(data).T
+        x_dot = d_dt(data[:-input_dim]).T
 
         # prepare for the library
         lib, self._desp = self.polynomial_expansion(data.T, degree=poly_degree)
@@ -42,3 +52,36 @@ class SINDy(SINDyBase):
         self._coef, _ = self.sparsify_dynamics(lib, x_dot, cut_off)
 
         return self
+
+    def rhs(self, q_data):
+        theta_mat, self._desp = self.polynomial_expansion(q_data,
+                                                          degree=self._deg)
+        x_dot = theta_mat.dot(self._coef)
+        return x_dot
+
+    def predict(self, x0, u_arr, t_arr):
+        u_callback = interp1d(t_arr,
+                              u_arr,
+                              axis=1,
+                              kind="linear",
+                              bounds_error=False,
+                              fill_value=(u_arr[..., 0], u_arr[..., -1])
+                              )
+
+        def _wrapper(t, x):
+            u = u_callback(t)
+            q = np.atleast_2d(np.hstack((x, u)))
+            x_dot = self.rhs(q)
+            return x_dot.squeeze()
+
+        np.seterr(under="warn")
+        result = solve_ivp(_wrapper,
+                           t_span=(t_arr[0], t_arr[-1]),
+                           t_eval=t_arr,
+                           y0=x0,
+                           rtol=1e-6,
+                           atol=1e-6,
+                           )
+        if not result.success:
+            logger.error(result.message)
+        return result.y
