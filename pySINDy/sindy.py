96ef7339b6f017c54f39a2e6e818df9b07f1e522
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2019-07-19 13:29:33 +0200

    Changed input shapes to more sane approach (time first)

--- pySINDy/sindy.py
+++ pySINDy/sindy.py
@@ -17,18 +17,16 @@ class SINDy(SINDyBase):
     Sparse Identification of Nonlinear Dynamics:
     reference: http://www.pnas.org/content/pnas/113/15/3932.full.pdf
     """
-    def fit(self, data, _dt, poly_degree=2, cut_off=1e-3, deriv_acc=2,
-            input_dim=0):
+    def fit(self, data, t_grid, poly_degree=2, cut_off=1e-3, deriv_order=2,
+            input_data=None):
         """
         :param data: dynamics data to be processed
-        :param _dt: float, represents grid spacing
+        :param t_grid: float, represents grid spacing
         :param poly_degree: degree of polynomials to be included in theta matrix
         :param cut_off: the threshold cutoff value for sparsity
-        :param deriv_acc: (positive) integer, derivative accuracy
+        :param deriv_order: (positive) integer, derivative accuracy
         :return: a SINDy model
         """
-        self._deg = poly_degree
-        self._inp_dim = input_dim
 
         if len(data.shape) == 1:
             data = data[np.newaxis, ]
@@ -40,16 +38,24 @@ class SINDy(SINDyBase):
             print("The array is converted to 2D automatically: in SINDy, "
                   "each dimension except for the time (default the last dimension) "
                   "are treated equally.")
+        dt = t_grid[1:] - t_grid[:-1]
+        if np.allclose(dt, dt[0]):
+            args = (0, dt[0], 1)
+        else:
+            args = (0, t_grid, 1)
 
         # compute time derivative
-        d_dt = FinDiff(data.ndim-1, _dt, 1, acc=deriv_acc)
-        x_dot = d_dt(data[:-input_dim]).T
+        d_dt = FinDiff(args, acc=deriv_order)
+        x_dot = d_dt(data)
 
         # prepare for the library
-        lib, self._desp = self.polynomial_expansion(data.T, degree=poly_degree)
+        lib, self._desp = self.polynomial_expansion(data, degree=poly_degree)
 
         # sparse regression
         self._coef, _ = self.sparsify_dynamics(lib, x_dot, cut_off)
+
+        # identify non trivial terms and remember them
+        self._deg = poly_degree
 
         return self
 
--- pySINDy/tests/test_sindy.py
+++ pySINDy/tests/test_sindy.py
@@ -1,12 +1,50 @@
 # pylint: skip-file
 
-from unittest import TestCase
-from ..sindy import SINDy
-import numpy as np
 import warnings
+from unittest import TestCase
 
+import numpy as np
+from scipy.integrate import solve_ivp
+
+from ..sindy import SINDy
 
 class TestSINDy(TestCase):
+    def test_shape1(self):
+        warnings.simplefilter("ignore")
+        model = SINDy()
+        data = np.random.rand(10,)
+        dt = 0.1
+        model.fit(data, dt)
+        assert True
+
+
+class TestSINDyC(TestCase):
+
+    def setUp(self):
+
+        self.u_func = np.sin
+
+        def rhs(t,y):
+            u = self.u_func(t)
+            y_dt = np.array([y[1], -y[0] + u])
+            return y_dt
+
+        t0 = 0
+        te = 10
+        t_grid = np.linspace(t0, te, 100)
+        y0 = np.array([1, 0])
+        res = solve_ivp(rhs, (t0, te), y0, t_grid=t_grid)
+
+        self.t_data = t_grid
+        self.y_data = res.y.T
+        self.u_data = self.u_func(t_grid)
+
+    def test_fit(self):
+        model = SINDy()
+        model.fit(self.y_data, self.t_data,
+                  poly_degree=1, input_data=self.u_data)
+        print(model._coef)
+
     def test_shape1(self):
         warnings.simplefilter("ignore")
         model = SINDy()
